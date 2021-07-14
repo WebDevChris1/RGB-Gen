d5eaba383f5a98ff3045791fb597a345a31d275a
danger2123 <sellmysolenyc@gmail.com>
2021-07-14 18:21:17 -0400

    finished JS

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const body = document.getElementById("bg");
-// body.setAttribute("style", "background-color: red");
-console.log(body);
+const btn = document.getElementById("btn");
+const rgbText = document.getElementById("rgb-text");
+let colors = 255;
+
+btn.addEventListener("click", function () {
+  let r = randomNum();
+  let g = randomNum();
+  let b = randomNum();
+  body.setAttribute("style", `background-color: rgb(${r}, ${g}, ${b})`);
+  rgbText.textContent = `rgb(${r}, ${g}, ${b})`;
+});
+
+function randomNum() {
+  let num = Math.floor(Math.random() * colors);
+  return num;
+}
